b00811382985bbcb34800aeb29e4e96a3f474f05
Rohit Chalak <rohit.s.chalak@gmail.com>
2023-07-06 02:58:36 +0530

    project complete

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,11 +1,16 @@
 import styles from "./styles.module.css";
+import RoomAndUsersColumn from "./room-and-users";
+import SendMessage from "./send-message";
 import MessagesReceived from "./messages";
 
-const Chat = ({ socket }) => {
+const Chat = ({ username, room, socket }) => {
   return (
     <div className={styles.chatContainer}>
+      <RoomAndUsersColumn socket={socket} username={username} room={room} />
+
       <div>
         <MessagesReceived socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
       </div>
     </div>
   );
